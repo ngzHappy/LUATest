6843f8e617fc22083d0bcb92e98d47bbcfded6ed
ngzHappy <zhaixueqiang@hotmail.com>
2015-11-19 20:24:32 +0800

    version 0.1

--- MainTest.cpp
+++ MainTest.cpp
@@ -1,6 +1,7 @@
 ﻿#include "MainTest.hpp"
 #include <QApplication>
 #include <lua/LuaEngine.hpp>
+#include <cstdlib>
 
 MainTest::MainTest()
 {
@@ -11,7 +12,7 @@ MainTest::MainTest()
 
 int xxxtest( lua_State * s ) {
     std::cout<<"xxxtest"<<std::endl;
-    
+
     lua_pushnumber(s,123);
     return 1;
 }
@@ -29,14 +30,14 @@ int Test_Construct( lua_State *L ) {
     new(userData) std::shared_ptr<Test>( test );
 
     /* 设置元表 */
-    luaL_getmetatable(L, "@@Test");  
-    lua_setmetatable(L,-2);  
+    luaL_getmetatable(L, "@@Test");
+    lua_setmetatable(L,-2);
 
     return 1;
 }
 
 template<typename T>
-void destory( T * d ) { 
+void destory( T * d ) {
     d->~T();
 }
 
@@ -45,11 +46,11 @@ int Test_Destruct(lua_State *L) {
     return 0;
 }
 
-int Test_Print(lua_State *L) {
+int Test_Print(lua_State * ) {
     std::cout<<"test print"<<std::endl;
     return 0;
 }
- 
+
 template<typename T_>class FunctionPointer {
 public:
     const char * functionName;
@@ -60,18 +61,21 @@ public:
         functionPointer(fp) {}
 };
 
-int main(int argc,char ** argv)  { 
-      
+int main(int argc,char ** argv)  {
+
 
 QApplication app( argc,argv );
 
+MainTest test;
+test.show();
+{
     LuaEngine engine ;
     std::cout<<engine.getTop()<<std::endl;
     engine.eval("print(12+mathsin(12))" );
     engine.eval("abc=12");
     engine.setGlobalNumber("fabc",22);
     std::cout<< engine.getGlobalNumber("fabc") <<std::endl ;
-    
+
     engine.setGlobalString("fstr","string");
     std::cout<<engine.getGlobalString("fstr")<<std::endl;
 
@@ -80,22 +84,22 @@ QApplication app( argc,argv );
     std::cout << engine.getGlobalCFunction("xtest") <<std::endl ;
 
     auto * L=engine.getLuaState();
-    std::cout << luaL_newmetatable(L,"@@Test") <<std::endl;  
+    std::cout << luaL_newmetatable(L,"@@Test") <<std::endl;
 
-    //__gc是lua默认的清理函数  
-    lua_pushstring(L,"__gc");  
-    lua_pushcfunction(L,&Test_Destruct);  
-    lua_settable(L,-3);//公共函数调用的实现就在此啊  
+    //__gc是lua默认的清理函数
+    lua_pushstring(L,"__gc");
+    lua_pushcfunction(L,&Test_Destruct);
+    lua_settable(L,-3);//公共函数调用的实现就在此啊
 
-    lua_pushstring(L,"xprint");  
-    lua_pushcfunction(L,&Test_Print);  
-    lua_settable(L,-3);//公共函数调用的实现就在此啊  
+    lua_pushstring(L,"xprint");
+    lua_pushcfunction(L,&Test_Print);
+    lua_settable(L,-3);//公共函数调用的实现就在此啊
 
-    lua_pushstring(L,"__index");  
-    lua_pushvalue(L,-2);  
-    lua_settable(L,-3);  
-       
-    lua_pop(L,1);  
+    lua_pushstring(L,"__index");
+    lua_pushvalue(L,-2);
+    lua_settable(L,-3);
+
+    lua_pop(L,1);
 
     engine.setGlobalCFunction("Test",&Test_Construct);
 
@@ -104,25 +108,25 @@ QApplication app( argc,argv );
     engine.eval( " xxyy() " );
 
     std::cout<<*engine.getGlobalStdString("yyyd")<<std::endl;
-    
+
   //  lua_gc(L, LUA_GCCOLLECT, LUA_GCSETSTEPMUL);
 
     std::cout<<engine.getTop()<<std::endl;
-    MainTest test;
-    test.show();
 
     std::cout<<LuaEngine::Register<int>::index()<<std::endl;
     std::cout<<LuaEngine::Register<int>::index()<<std::endl;
     std::cout<<LuaEngine::Register<double>::index()<<std::endl;
     std::cout<<LuaEngine::Register<LuaStdString>::index()<<std::endl;
-    
-return app.exec();
 
-#ifdef _MSC_VER
-system("pause");
-#endif // _MSC_VER
+}
+
+{
+    auto ans = app.exec() ;
+    return ans;
+}
+
 
 }
 
 
-//type meta_table_name 
+//type meta_table_name
--- lua/LuaEngine.hpp
+++ lua/LuaEngine.hpp
@@ -18,8 +18,12 @@ class LuaEngineData{
 public:
     lua_State * state = nullptr ;
     std::thread::id threadID;
-    LuaEngineData();
+    static std::shared_ptr<LuaEngineData> instanceData();
     ~LuaEngineData();
+
+private:
+    LuaEngineData();
+
 };
 
 class LuaUserData {
@@ -27,14 +31,14 @@ public:
     typedef std::size_t size_t;
     static size_t invalid() { return -1; }
     size_t typeID = invalid();
-    bool const isInvalidLuaUserData() const{
+    bool  isInvalidLuaUserData() const{
         return typeID!=invalid();
     }
     LuaUserData() {}
 };
 
 template<typename T>
-class LuaPointer : 
+class LuaPointer :
     public LuaUserData,
     public std::shared_ptr<T>{
 public:
@@ -48,13 +52,21 @@ public:
 class LuaEngine
 {
     std::shared_ptr<LuaEngineData>  ldata  ;
-    static std::shared_ptr<LuaEngineData> instanceData();
-    static LuaUserData::size_t & getIndexID_() { 
+    static std::shared_ptr<LuaEngineData> instanceData(){
+        return LuaEngineData::instanceData();
+    }
+    static LuaUserData::size_t & getIndexID_() {
         static LuaUserData::size_t typeIndex_ =0;
         return typeIndex_;
     }
+
 public:
-    
+
+    static std::thread::id mainThreadID(){
+        static std::thread::id ans_ = std::this_thread::get_id();
+        return ans_;
+    }
+
     enum class Status : int {
         /* thread status */
 OK         =  LUA_OK		,
@@ -101,10 +113,10 @@ ERRERR     =  LUA_ERRERR	,
     std::string getGlobalString(const char *) const ;
     void setGlobalString(const char * n, const std::string & v) {  setGlobalString(n,v.c_str()); }
     void setGlobalString(const char * n, std::string && v) { std::string v_=std::move(v); setGlobalString(n,v_.c_str()); }
-    
+
     void setGlobalCFunction( const char * , lua_CFunction function );
     lua_CFunction getGlobalCFunction( const char * ) const;
-    
+
     LuaPointer< LuaStdString > getGlobalStdString(const char * n) const;
 
 public:
